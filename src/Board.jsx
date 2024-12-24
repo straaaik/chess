a68fdb56067d28935f7f527129bd440dc930ba23
stk <igortimst@gmail.com>
2024-12-24 18:33:28 +0300

    #

--- src/Board.jsx
+++ src/Board.jsx
@@ -63,15 +63,25 @@ export default function Board() {
         }
       })
     );
+    e.target.parentElement.style.background = "rgba(255, 0, 0, 0.207)";
     setMemoryBoard(newBoard);
     setMemoryCell(cell);
   };
   const dragEndHandler = (e) => {
+    console.log("1");
     e.target.style.background = "";
+    e.target.parentElement.style.background = "";
   };
   const dragOverHandler = (e) => {
     e.preventDefault();
-    e.target.style.background = "rgba(255, 0, 0, 0.207)";
+    if (e.target.className == "figure")
+      e.target.parentElement.style.background = "rgba(255, 0, 0, 0.207)";
+    else e.target.style.background = "rgba(255, 0, 0, 0.207)";
+  };
+
+  const dragLeaveHandler = (e) => {
+    e.target.style.background = "";
+    e.target.parentElement.style.background = "";
   };
 
   const dropHandler = (e, cell) => {
@@ -85,7 +95,9 @@ export default function Board() {
         }
       })
     );
+    console.log(2);
     e.target.style.background = "";
+    e.target.parentElement.style.background = "";
     setBoard(newBoard);
   };
 
@@ -100,28 +112,30 @@ export default function Board() {
             <div
               key={cell.id.id}
               className={cell.color}
-              onDragLeave={(e) => dragEndHandler(e)}
-              onDragEnd={(e) => dragEndHandler(e)}
+              onDragLeave={(e) => dragLeaveHandler(e)}
               onDragOver={(e) => dragOverHandler(e)}
               onDrop={(e) => dropHandler(e, cell)}
             >
               <div className="id">
-                {cell.id.num == 1 ? cell.id.char.toLocaleUpperCase() : ""}
-                {cell.id.char == "a" ? cell.id.num : ""}
+                <div className="id-str">
+                  {cell.id.num == 1 ? cell.id.char.toLocaleUpperCase() : ""}
+                </div>
+                <div className="id-num">
+                  {cell.id.char == "a" ? cell.id.num : ""}
+                </div>
               </div>
-              <div>
-                <img
-                  onDragStart={(e) => dragStartHandler(e, cell)}
-                  onDragLeave={(e) => dragEndHandler(e)}
-                  onDragEnd={(e) => dragEndHandler(e)}
-                  onDragOver={(e) => dragOverHandler(e)}
-                  onDrop={(e) => dropHandler(e, cell)}
-                  draggable={true}
-                  className={cell.figure == null ? "" : "figure"}
-                  src={cell.figure}
-                  alt=""
-                />
-              </div>
+
+              <img
+                onDragStart={(e) => dragStartHandler(e, cell)}
+                onDragLeave={(e) => dragLeaveHandler(e)}
+                onDragEnd={(e) => dragEndHandler(e)}
+                onDragOver={(e) => dragOverHandler(e)}
+                onDrop={(e) => dropHandler(e, cell)}
+                draggable={true}
+                className={cell.figure == null ? "" : "figure"}
+                src={cell.figure}
+                alt=""
+              />
             </div>
           ))
         )}
